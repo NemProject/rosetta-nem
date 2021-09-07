46ba16cf8bbb29b09336f11c0dd2eec3a4be83a5
gimre <g.imre@protonmail.com>
2021-09-07 14:34:26 +0200

    mocking network

--- pyserver/src/openapi_server/apis/network_api.py
+++ pyserver/src/openapi_server/apis/network_api.py
@@ -24,6 +24,12 @@ from openapi_server.models.network_options_response import NetworkOptionsRespons
 from openapi_server.models.network_request import NetworkRequest
 from openapi_server.models.network_status_response import NetworkStatusResponse
 
+from openapi_server.models.allow import Allow
+from openapi_server.models.block_identifier import BlockIdentifier
+from openapi_server.models.network_identifier import NetworkIdentifier
+from openapi_server.models.peer import Peer
+from openapi_server.models.sync_status import SyncStatus
+from openapi_server.models.version import Version
 
 router = APIRouter()
 
@@ -41,7 +47,11 @@ async def network_list(
     metadata_request: MetadataRequest = Body(None, description=""),
 ) -> NetworkListResponse:
     """This endpoint returns a list of NetworkIdentifiers that the Rosetta server supports. """
-    ...
+    return NetworkListResponse(
+        network_identifiers=[
+            NetworkIdentifier(blockchain='NEM', network='mainnet')
+        ]
+    )
 
 
 @router.post(
@@ -57,7 +67,30 @@ async def network_options(
     network_request: NetworkRequest = Body(None, description=""),
 ) -> NetworkOptionsResponse:
     """This endpoint returns the version information and allowed network-specific types for a NetworkIdentifier. Any NetworkIdentifier returned by /network/list should be accessible here.  Because options are retrievable in the context of a NetworkIdentifier, it is possible to define unique options for each network. """
-    ...
+    return NetworkOptionsResponse(
+        version=Version(
+            rosetta_version='1.0.0',
+            node_version='123',
+
+        ),
+        allow=Allow(
+            operation_statuses=[
+                {
+                    "status": "SUCCESS",
+                    "successful": True
+                }
+            ],
+            operation_types=[
+                "TRANSFER"
+            ],
+            errors=[],
+            historical_balance_lookup=False,
+            timestamp_start_index=1,
+            call_methods=[],
+            balance_exemptions=[],
+            mempool_coins=False # ...
+        )
+    )
 
 
 @router.post(
@@ -73,4 +106,28 @@ async def network_status(
     network_request: NetworkRequest = Body(None, description=""),
 ) -> NetworkStatusResponse:
     """This endpoint returns the current status of the network requested. Any NetworkIdentifier returned by /network/list should be accessible here. """
-    ...
+    if network_request.network_identifier != NetworkIdentifier(blockchain='NEM', network='mainnet'):
+        return Error(code=1, message='invalid network', retriable=True)
+
+    current_block_identifier=BlockIdentifier(
+        index = 1,
+        hash = '1234567890abcdef'
+    )
+    genesis_block_identifier=BlockIdentifier(
+        index = 1,
+        hash = '1234567890abcdef'
+    )
+
+    return NetworkStatusResponse(
+        current_block_identifier=current_block_identifier,
+        current_block_timestamp=1615853185000,
+        genesis_block_identifier=genesis_block_identifier,
+        #oldest_block_identifier=
+        sync_status=SyncStatus(
+                current_index=1,
+                target_index=0,
+                synced=True
+        ),
+        peers=[
+        ]
+    )
